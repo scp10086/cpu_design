11572a0f70b5e5554d55f6d81a0708ebdb06e615
scp10086 <2632711141@qq.com>
2020-05-25 14:06:03 +0800

    Update MBR.v

--- src/MBR.v
+++ src/MBR.v
@@ -23,6 +23,55 @@
 module MBR(
 input clk,
 input rst,
-input [31:0]
+input [31:0]control_signal,
+input [15:0]data_from_memory,
+input [7:0]data_from_pc,
+input [15:0]data_from_mr,
+input [15:0]data_from_acc,
+output reg [15:0]data_to_memory,
+output reg [7:0]data_to_pc,
+output reg [7:0]data_to_mar,
+output reg [7:0]data_to_ir,
+output reg [15:0]data_to_br
     );
+reg [15:0]buffer_mbr;
+always@(posedge clk or negedge rst)begin
+    if(!rst)begin
+        data_to_memory<=0;
+        data_to_pc<=0;
+        data_to_mar<=0;
+        buffer_mbr <=0;
+        data_to_ir<=0;
+        data_to_br<=0;
+    end
+    else begin
+        if(control_signal[5] == 1)begin
+            buffer_mbr<=data_from_memory;
+        end
+        if(control_signal[12] == 1)begin
+            data_to_memory<=buffer_mbr;
+        end
+        if(control_signal[8] == 1)begin
+            data_to_mar<=buffer_mbr[7:0];
+        end
+        if(control_signal[3] == 1)begin
+            data_to_pc<=buffer_mbr[7:0];
+        end
+        if(control_signal[1] == 1)begin
+            buffer_mbr[7:0]<=data_from_pc;
+        end      
+        if(control_signal[4] == 1)begin
+            data_to_ir<=buffer_mbr[15:8];
+        end 
+        if(control_signal[6] == 1)begin
+            data_to_br<=buffer_mbr;
+        end 
+        if(control_signal[15] == 1)begin
+            buffer_mbr<=data_from_mr;
+        end
+        if(control_signal[11] == 1)begin
+            buffer_mbr<=data_from_acc;
+        end                                        
+    end
+end
 endmodule

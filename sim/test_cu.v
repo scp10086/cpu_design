9bd8441a03f2fcbc64eda97db95218c1002f267c
scp10086 <2632711141@qq.com>
2020-05-26 03:57:22 +0800

    add some thing

--- sim/test_cu.v
+++ sim/test_cu.v
@@ -25,15 +25,30 @@ reg clk;
 reg rst;
 reg [7:0]ir2cu;
 reg [7:0]flags;
+wire [15:0]mbr2ram;
+reg [7:0]addr;
+wire [15:0]ram2mbr;
+reg [7:0]pc2mbr;
 wire[31:0]control_signal;
+reg [15:0]mr2mbr;
+reg [15:0]acc2mbr;
+wire [7:0]mbr2pc;
+wire [7:0]mbr2mar;
+wire [7:0]mbr2ir;
+wire [7:0]mbr2br;
 initial begin
 clk<=0;
 rst<=0;
 flags<=0;
 ir2cu<=8'b0000_0010;
-
+pc2mbr<=1;
+addr<=0;
+mr2mbr<=2;
+acc2mbr<=3;
 # 60
 rst<=1;
+# 100
+addr<=8'ha1;
 end
 always # 10 clk=~clk;
 cu u_cu(
@@ -42,5 +57,27 @@ cu u_cu(
 .data_from_ir(ir2cu),
 .flags(flags),
 .control_signal(control_signal)
+);
+RAM u_RAM(
+.clk(clk),
+.rst(rst),
+.control_signal(control_signal),
+.data_in(mbr2ram),
+.data_out(ram2mbr),
+.addr(addr)
+);
+MBR u_MBR(
+.clk(clk),
+.rst(rst),
+.control_signal(control_signal),
+.data_from_memory(ram2mbr),
+.data_from_pc(pc2mbr),
+.data_from_mr(mr2mbr),
+.data_from_acc(acc2mbr),
+.data_to_memory(mbr2ram),
+.data_to_pc(mbr2pc),
+.data_to_mar(mbr2mar),
+.data_to_ir(mbr2ir),
+.data_to_br(mbr2br)
 );
 endmodule
